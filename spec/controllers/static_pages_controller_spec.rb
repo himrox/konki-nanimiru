5bba0a88dcc319f123be591e85c96170d22a7554
himrox <himrox77@gmail.com>
2017-04-22 20:54:15 +0900

    StaticPagesコントローラーのテストを追加

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
 
+  describe 'GET #home' do
+    it 'homeテンプレートを表示すること' do
+      get :home
+      expect(response).to render_template :home
+    end
+  end
+
 end
