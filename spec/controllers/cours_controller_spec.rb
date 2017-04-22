671fd34b3571494df2082e0f617d41ac7790397b
himrox <himrox77@gmail.com>
2017-04-22 20:37:04 +0900

    coursコントローラーのテストを追加

--- spec/controllers/cours_controller_spec.rb
+++ spec/controllers/cours_controller_spec.rb
@@ -2,10 +2,19 @@ require 'rails_helper'
 
 RSpec.describe CoursController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
+  describe 'GET #index' do
+    it 'Indexテンプレートを表示すること' do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to render_template :index
+    end
+
+    it '全てのクールを配列にまとめること' do
+      (1..4).each do |i|
+        create(:cour, season: i)
+      end
+      cours = Cour.all.order(:year, :season)
+      get :index
+      expect(assigns(:cours)).to eq(cours)
     end
   end
 
