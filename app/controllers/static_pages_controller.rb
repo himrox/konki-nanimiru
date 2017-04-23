26b32246fdbc17ddaaafb9afb0a3bf8de410f29b
himrox <himrox77@gmail.com>
2017-04-23 22:53:48 +0900

    レイアウトを修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,23 @@
 class StaticPagesController < ApplicationController
   def home
+    year = Date.today.year
+    mon = Date.today.mon
+    @cour = Cour.find_by(year: year, season: season(mon))
+    @animes = @cour.animes
+  end
+
+  private
+
+  def season(mon)
+    case mon
+    when 1..3
+      1
+    when 4..6
+      2
+    when 7..9
+      3
+    when 10..12
+      3
+    end
   end
 end
