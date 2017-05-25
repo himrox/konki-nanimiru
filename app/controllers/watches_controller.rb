ccfb535d60cd1c0d5d79d980ab0e927ffda22bd9
himrox <himrox77@gmail.com>
2017-05-25 22:27:59 +0900

    Watchコントローラーのjsファイルを修正

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -6,10 +6,6 @@ class WatchesController < ApplicationController
     user_id = current_user.id
     @anime_id = params[:anime_id]
     Watch.find_or_create_by(user_id: user_id, anime_id: @anime_id)
-    respond_to do |format|
-      format.html
-      format.js
-    end
   end
 
   def destroy
@@ -17,9 +13,5 @@ class WatchesController < ApplicationController
     @anime_id = params[:anime_id]
     watch = Watch.find_by(user_id: user_id, anime_id: @anime_id)
     watch.destroy
-    respond_to do |format|
-      format.html
-      format.js
-    end
   end
 end
