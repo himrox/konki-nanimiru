b725910a1ede67b52b5c188dc96997a56601b195
himrox <himrox77@gmail.com>
2017-06-06 21:53:54 +0900

    Rspecのsupportファイルを整理

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,7 +55,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  config.include FactoryGirl::Syntax::Methods
-  config.include Devise::Test::ControllerHelpers, :type => :controller
-  config.include ControllerMacros, :type => :controller
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,27 +40,6 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-  # Database cleaner
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before(:each, :js => true) do
-    DatabaseCleaner.strategy = :truncation
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
   # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
   # have no way to turn it off -- the option exists only for backwards
   # compatibility in RSpec 3). It causes shared context metadata to be
