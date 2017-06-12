6ed77c02968e6eafc0418c014616574338ae5489
himrox <himrox77@gmail.com>
2017-06-12 22:53:09 +0900

    現在のクールを返すHelperメソッドを追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    year = Date.today.year
-    mon = Date.today.mon
-    cour = Cour.find_by(year: year, season: season(mon))
-    @animes = cour.animes.order(:api_number) unless cour.nil?
+    @animes = current_cour.animes.order(:api_number) unless current_cour.nil?
     @watch_animes = current_user.animes if user_signed_in?
-  end
-
-  private
-
-  def season(mon)
-    (mon / 3.0).ceil
   end
 end
