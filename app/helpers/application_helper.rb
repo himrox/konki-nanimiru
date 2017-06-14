75252006cbf3715255c7df77956cf71eb0c9c882
himrox <himrox77@gmail.com>
2017-06-14 18:57:34 +0900

    不要な行の削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,3 @@
 module ApplicationHelper
-  # 与えられたユーザーが見ているアニメのidの配列を返す
-  # def watching_anime_ids(user)
-  #   ids = []
-  #   user.animes.each do |anime|
-  #     ids << anime.id
-  #   end
-  #   ids
-  # end
+
 end
