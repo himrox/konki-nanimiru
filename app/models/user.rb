6e509c6742e1c10992ddca2a8976ee6f123d13e9
himrox <himrox77@gmail.com>
2017-06-17 17:16:28 +0900

    twitterのOmniAuth認証でユーザーを作成する機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,10 +22,20 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:twitter]
 
   has_many :watches, dependent: :destroy
   has_many :animes, through: :watches
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = "#{auth.info.name}@twitter.com"
+      user.password = Devise.friendly_token[0,20]
+      # user.name = auth.info.name
+      # user.image = auth.info.image
+    end
+  end
 
   # ユーザーが見ているアニメのidの配列を返す
   def watching_anime_ids
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -250,7 +250,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :twitter, 'MH8PZFb5QyavWGPQHt8sjU48P', 'POCQT3H8jaqWxo1wrYkqMeX1WXjo3nloPcOKvLyhxgrHpMzRmP'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
