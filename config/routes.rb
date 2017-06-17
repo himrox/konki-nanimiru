3145b61b827146a39f6874d457f329cdd78da64e
himrox <himrox77@gmail.com>
2017-06-17 23:05:23 +0900

    usernameの入力欄を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => {
+    :omniauth_callbacks => "users/omniauth_callbacks",
+    :registrations => "users/registrations",
+    :sessions => "users/sessions"
+  }
   root 'static_pages#home'
   resources :animes, only: [:edit, :update]
   get '/animes/:year/:season', to: 'animes#index', as: 'animes'
