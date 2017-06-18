1bd60ec207c610ff5dea8f047c087c99371ff432
himrox <himrox77@gmail.com>
2017-06-18 15:46:56 +0900

    過去のアニメへのリンクを自動で生成するように変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     base_title = '今期なに見る？'
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
   end
+
+  def last_3_cours
+    Cour.order(year: :desc, season: :desc).limit(3).offset(1)
+  end
 end
