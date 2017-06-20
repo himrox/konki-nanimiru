867ddc57b7d5d32c56ef4c79d97355b0707c428d
himrox <himrox77@gmail.com>
2017-06-20 22:45:18 +0900

    マイページに今期見ているアニメの一覧を追加

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,5 +1,6 @@
 class MypagesController < ApplicationController
   def show
     @user = User.find_by(username: params[:username])
+    @animes = @user.animes.where(cour_id: Cour.current_cour.id)
   end
 end
